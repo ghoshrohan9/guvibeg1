978b3067412109883dcbc9134fd942f1b16077d7
ghoshrohan9 <rohfriend@gmail.com>
2017-07-16 11:43:42 +0530

    Positive or Negative number

---
+++ pnz.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class rohan
+{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		double n=sc.nextDouble();
+		if(n==0) System.out.println("ZERO");
+		else if(n>0) System.out.println("POSITIVE");
+		else System.out.println("NEGATIVE");
+	}
+}
